cef2f28e4292bf859e52010df2a3f518730b4f30
Hasan Parasteh <hasanparasteh@gmail.com>
2022-05-14 14:05:15 +0430

    chore: update globalHelper

--- src/Helpers/GlobalHelper.php
+++ src/Helpers/GlobalHelper.php
@@ -2,6 +2,8 @@
 
 namespace App\Helpers;
 
+use Psr\Http\Message\ServerRequestInterface;
+
 class GlobalHelper
 {
     public static function getCurrentMicroTime(): float
@@ -9,19 +11,32 @@ class GlobalHelper
         return round(microtime(true) * 1000);
     }
 
-    public static function generateRandomOtp(): float
+    public static function parseClientIp(ServerRequestInterface $request): string
     {
-        return rand(11111, 99999);
-    }
+        $serverParams = $request->getServerParams();
+        $headerParams = $request->getHeaders();
 
-    public static function parseClientIp(array $serverParams): string
-    {
         if (!empty($serverParams['HTTP_CLIENT_IP'])) {
             return $serverParams['HTTP_CLIENT_IP'];
         }
-        if (!empty($serverParams['HTTP_X_FORWARDED_FOR'])) {
-            return $serverParams['HTTP_X_FORWARDED_FOR'];
+        if (!empty($headerParams['X-Forwarded-For'][0])) {
+            return $headerParams['X-Forwarded-For'][0];
         }
         return $serverParams['REMOTE_ADDR'];
+    }
+
+    public static function generateUUID(): array
+    {
+        try {
+            $data = random_bytes(16);
+            $data[6] = chr(ord($data[6]) & 0x0f | 0x40);
+            $data[8] = chr(ord($data[8]) & 0x3f | 0x80);
+            return [
+                'result' => true,
+                'hash' => vsprintf('%s%s%s%s%s%s%s%s', str_split(bin2hex($data), 4))
+            ];
+        } catch (\Exception $e) {
+            return ['result' => false];
+        }
     }
 }
